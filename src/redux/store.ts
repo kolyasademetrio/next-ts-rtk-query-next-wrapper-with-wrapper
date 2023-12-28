988067bbe40d6c35f2db2ac64dddc3dcee2ffb4c
dmytro.koliasa@onpoint.to <dmytro.koliasa@onpoint.to>
2023-12-28 18:03:42 +0200

    added makeStore within createReducerManager

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,63 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { testReducer } from "./testSlice";
+
+// export const store = configureStore({
+//    reducer: {
+//       test: testReducer,
+//    },
+//    devTools: process.env.NODE_ENV === "development",
+//    middleware: getDefaultMiddleware => getDefaultMiddleware({}),
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+import {
+   Action,
+   CombinedState,
+   configureStore,
+   Reducer,
+   ReducersMapObject,
+   ThunkAction,
+} from "@reduxjs/toolkit";
+import { Context, createWrapper } from "next-redux-wrapper";
+import { StoreSchema } from "./types/store";
 import { testReducer } from "./testSlice";
+import { createReducerManager } from "./createReducerManager";
+// import { $api } from '@/shared/api/api';
+// import { rtkApi } from '@/shared/api/rtkApi';
 
-export const store = configureStore({
-   reducer: {
+export const makeStore = (
+   initialState?: StoreSchema,
+   context?: Context,
+   asyncReducers?: ReducersMapObject<StoreSchema>
+) => {
+   const rootReducer: ReducersMapObject<StoreSchema> = {
       test: testReducer,
-   },
-   devTools: process.env.NODE_ENV === "development",
-   middleware: getDefaultMiddleware => getDefaultMiddleware({}),
-});
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+      // [rtkApi.reducerPath]: rtkApi.reducer,
+      ...asyncReducers,
+   };
+
+   const reducerManager = createReducerManager(rootReducer);
+
+   const store = configureStore({
+      reducer: reducerManager.reduce as Reducer<CombinedState<StoreSchema>>,
+      devTools: process.env.NODE_ENV === "development",
+      preloadedState: initialState,
+      middleware: getDefaultMiddleware => getDefaultMiddleware({}) /*.concat(rtkApi.middleware)*/,
+   });
+
+   // @ts-ignore
+   store.reducerManager = reducerManager;
+   return store;
+};
+
+export const store = makeStore();
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = ReturnType<typeof makeStore>["dispatch"];
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
+
+export const wrapper = createWrapper<AppStore>(context => makeStore(undefined, context));

1c55f4958999d018e401ec911234d41b77dad04d
dmytro.koliasa@onpoint.to <dmytro.koliasa@onpoint.to>
2023-12-31 13:19:58 +0200

    added next-redux-wrapper usage

--- src/redux/rtkApi.ts
+++ src/redux/rtkApi.ts
@@ -1,9 +1,18 @@
 import { createApi } from "@reduxjs/toolkit/dist/query/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { HYDRATE } from "next-redux-wrapper";
 
 export const rtkApi = createApi({
    reducerPath: "api",
    tagTypes: [],
    baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com" }),
+   // it is for next-redux-wrapper settings
+   extractRehydrationInfo(action, { reducerPath }) {
+      if (action.type === HYDRATE) {
+         return action.payload[reducerPath];
+      }
+   },
    endpoints: () => ({}),
 });
+
+export const { getRunningQueriesThunk } = rtkApi.util;
